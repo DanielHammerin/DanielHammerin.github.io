5a4a6cbeba9287615620f8721c90fd5cf253b88e
Daniel Hammerin <danne155@hotmail.com>
2021-05-18 13:41:41 +0200

    initial gh-pages commit

--- gh-pages.js
+++ gh-pages.js
@@ -1,7 +1,7 @@
 import ghpages from 'gh-pages';
 
 ghpages.publish(
-    'public', // path to public directory
+    './public', // path to public directory
     {
         branch: 'gh-pages',
         repo: 'https://github.com/DanielHammerin/DanielHammerin.github.io.git', // Update to point to your repository  
